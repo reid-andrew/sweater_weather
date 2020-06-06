6fb0b5e8e019c3adad197effe5233291e89ab44b
Andrew Reid <areid.sj@gmail.com>
2020-06-06 10:36:38 -0600

    Write and pass test for returning photo

--- app/services/photo_service.rb
+++ app/services/photo_service.rb
@@ -2,6 +2,11 @@ class PhotoService
   class << self
     include Parseable
 
+    def find_photo(location)
+      reference = find_photo_reference(location)
+      photo_connection(reference).body
+    end
+
     def find_photo_reference(location)
       place = parse_json(connection(location))
       place[:candidates][0][:photos][0][:photo_reference]
@@ -16,6 +21,15 @@ class PhotoService
         req.params['key'] = ENV['GOOGLE_GEOCODING_KEY']
         req.params['inputtype'] = 'textquery'
         req.params['fields'] = 'photos'
+      end
+    end
+
+    def photo_connection(reference)
+      url = 'https://maps.googleapis.com/maps/api/place/photo'
+      Faraday.get(url) do |req|
+        req.params['maxwidth'] = 1600
+        req.params['photoreference'] = reference
+        req.params['key'] = ENV['GOOGLE_GEOCODING_KEY']
       end
     end
   end
--- spec/features/services/photo/photo_spec.rb
+++ spec/features/services/photo/photo_spec.rb
@@ -12,5 +12,19 @@ RSpec.describe 'Photo Service: ', type: :feature do
 
       expect(PhotoService.find_photo_reference(location)).to eq(expected_reference)
     end
+
+    it 'I provide a location I get back a photo for that place' do
+      location = 'Cleveland, OH'
+
+      place_response = File.read('spec/fixtures/photo_service/cleveland_oh.json')
+      stub_request(:get, "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Cleveland, OH&inputtype=textquery&fields=photos&key=#{ENV['GOOGLE_GEOCODING_KEY']}")
+                  .to_return(status: 200, body: place_response, headers: {})
+
+      photo_response = File.read('spec/fixtures/photo_service/94eaba86-e0a6-4d9f-a05f-9743b5380549-response.jpg')
+      stub_request(:get, "https://maps.googleapis.com/maps/api/place/photo?maxwidth=1600&key=#{ENV['GOOGLE_GEOCODING_KEY']}&photoreference=CmRaAAAAfny913NNeO6SIBfVvvdIiXGEeRAxWQH1Ffv2Z6MFTtth8IflrIovI04rhX_f9LigLheAaitXkfiTjkZA473D4KduNoXZ_GWYgX_RYlE7H15CKBwg1p94u2W_JXLho4SfEhC4_tHXZZATxbYymk4mPkqJGhRSpoiXvVKvaozNd58bzu066fU5hA")
+                  .to_return(status: 200, body: photo_response, headers: {})
+
+      expect(PhotoService.find_photo(location)).to eq(photo_response)
+    end
   end
 end
