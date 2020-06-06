b5a3f7a8dd558e5ff0fa0418121d06a6f102acd4
Andrew Reid <areid.sj@gmail.com>
2020-06-06 15:32:51 -0600

    Refactor test to shorten assertions

--- spec/requests/api/v1/forecasts/forecast_spec.rb
+++ spec/requests/api/v1/forecasts/forecast_spec.rb
@@ -17,76 +17,77 @@ RSpec.describe 'Forecast Endpoint -', type: :request do
     @geocode = JSON.parse(geocode_fixture, symbolize_names: true)
 
     get '/api/v1/forecast?location=cleveland, oh'
-    @expected = JSON.parse(response.body)
+    @output = JSON.parse(response.body)
+    @expected = @output["data"]["attributes"]["weather"]
   end
 
   it 'responds with success' do
     expect(response).to be_successful
-    expect(@expected["data"]["id"]).to eq("cleveland, oh - #{@weather[:current][:dt]}")
+    expect(@output["data"]["id"]).to eq("cleveland, oh - #{@weather[:current][:dt]}")
   end
 
   it 'provides location information for a city' do
-    expect(@expected["data"]["attributes"]["weather"]["location"][0]["city"]).to eq(@geocode[:results][0][:address_components][0][:long_name])
-    expect(@expected["data"]["attributes"]["weather"]["location"][0]["state"]).to eq(@geocode[:results][0][:address_components][2][:short_name])
-    expect(@expected["data"]["attributes"]["weather"]["location"][0]["country"]).to eq(@geocode[:results][0][:address_components][3][:long_name])
+    expect(@expected["location"][0]["city"]).to eq(@geocode[:results][0][:address_components][0][:long_name])
+    expect(@expected["location"][0]["state"]).to eq(@geocode[:results][0][:address_components][2][:short_name])
+    expect(@expected["location"][0]["country"]).to eq(@geocode[:results][0][:address_components][3][:long_name])
   end
 
   it 'provides current_weather for a city' do
-    expect(@expected["data"]["attributes"]["weather"]["current_weather"][0]["time"]).to eq(Time.at(@weather[:current][:dt]).strftime('%l:%M %p, %B%e'))
-    expect(@expected["data"]["attributes"]["weather"]["current_weather"][0]["current_temp"]).to eq(@weather[:current][:temp])
-    expect(@expected["data"]["attributes"]["weather"]["current_weather"][0]["high"]).to eq(@weather[:daily][0][:temp][:max])
-    expect(@expected["data"]["attributes"]["weather"]["current_weather"][0]["low"]).to eq(@weather[:daily][0][:temp][:min])
-    expect(@expected["data"]["attributes"]["weather"]["current_weather"][0]["description"]).to eq(@weather[:current][:weather][0][:description])
-    expect(@expected["data"]["attributes"]["weather"]["current_weather"][0]["image"]).to eq("http://openweathermap.org/img/wn/#{@weather[:current][:weather][0][:icon]}@2x.png")
+    expect(@expected["current_weather"][0]["time"]).to eq(Time.at(@weather[:current][:dt]).strftime('%l:%M %p, %B%e'))
+    expect(@expected["current_weather"][0]["current_temp"]).to eq(@weather[:current][:temp])
+    expect(@expected["current_weather"][0]["high"]).to eq(@weather[:daily][0][:temp][:max])
+    expect(@expected["current_weather"][0]["low"]).to eq(@weather[:daily][0][:temp][:min])
+    expect(@expected["current_weather"][0]["description"]).to eq(@weather[:current][:weather][0][:description])
+    expect(@expected["current_weather"][0]["image"]).to eq("http://openweathermap.org/img/wn/#{@weather[:current][:weather][0][:icon]}@2x.png")
   end
 
   it 'provides current_details for a city' do
-    expect(@expected["data"]["attributes"]["weather"]["current_details"][0]["description"]).to eq(@weather[:current][:weather][0][:description])
-    expect(@expected["data"]["attributes"]["weather"]["current_details"][0]["image"]).to eq("http://openweathermap.org/img/wn/#{@weather[:current][:weather][0][:icon]}@2x.png")
-    expect(@expected["data"]["attributes"]["weather"]["current_details"][0]["humidity"]).to eq(@weather[:current][:humidity])
-    expect(@expected["data"]["attributes"]["weather"]["current_details"][0]["visibility"]).to eq(@weather[:current][:visibility])
-    expect(@expected["data"]["attributes"]["weather"]["current_details"][0]["uv_index"]).to eq(@weather[:current][:uvi])
-    expect(@expected["data"]["attributes"]["weather"]["current_details"][0]["uv_index_interpreted"]).to eq('low')
-    expect(@expected["data"]["attributes"]["weather"]["current_details"][0]["sunrise"]).to eq(Time.at(@weather[:current][:sunrise]).strftime('%l:%M %p'))
-    expect(@expected["data"]["attributes"]["weather"]["current_details"][0]["sunset"]).to eq(Time.at(@weather[:current][:sunset]).strftime('%l:%M %p'))
-    expect(@expected["data"]["attributes"]["weather"]["current_details"][0]["feels_like"]).to eq(@weather[:current][:feels_like])
+    expect(@expected["current_details"][0]["description"]).to eq(@weather[:current][:weather][0][:description])
+    expect(@expected["current_details"][0]["image"]).to eq("http://openweathermap.org/img/wn/#{@weather[:current][:weather][0][:icon]}@2x.png")
+    expect(@expected["current_details"][0]["humidity"]).to eq(@weather[:current][:humidity])
+    expect(@expected["current_details"][0]["visibility"]).to eq(@weather[:current][:visibility])
+    expect(@expected["current_details"][0]["uv_index"]).to eq(@weather[:current][:uvi])
+    expect(@expected["current_details"][0]["uv_index_interpreted"]).to eq('low')
+    expect(@expected["current_details"][0]["sunrise"]).to eq(Time.at(@weather[:current][:sunrise]).strftime('%l:%M %p'))
+    expect(@expected["current_details"][0]["sunset"]).to eq(Time.at(@weather[:current][:sunset]).strftime('%l:%M %p'))
+    expect(@expected["current_details"][0]["feels_like"]).to eq(@weather[:current][:feels_like])
   end
 
   it 'provides hourly forecast for today' do
-    expect(@expected["data"]["attributes"]["weather"]["hourly_forecast"][0]["temp"]).to eq(@weather[:hourly][0][:temp])
-    expect(@expected["data"]["attributes"]["weather"]["hourly_forecast"][0]["image"]).to eq(@weather[:hourly][0][:weather][0][:icon])
-    expect(@expected["data"]["attributes"]["weather"]["hourly_forecast"][0]["time"]).to eq(Time.at(@weather[:hourly][0][:dt]).strftime('%l %p'))
+    expect(@expected["hourly_forecast"][0]["temp"]).to eq(@weather[:hourly][0][:temp])
+    expect(@expected["hourly_forecast"][0]["image"]).to eq(@weather[:hourly][0][:weather][0][:icon])
+    expect(@expected["hourly_forecast"][0]["time"]).to eq(Time.at(@weather[:hourly][0][:dt]).strftime('%l %p'))
 
-    expect(@expected["data"]["attributes"]["weather"]["hourly_forecast"][4]["temp"]).to eq(@weather[:hourly][4][:temp])
-    expect(@expected["data"]["attributes"]["weather"]["hourly_forecast"][4]["image"]).to eq(@weather[:hourly][4][:weather][0][:icon])
-    expect(@expected["data"]["attributes"]["weather"]["hourly_forecast"][4]["time"]).to eq(Time.at(@weather[:hourly][4][:dt]).strftime('%l %p'))
+    expect(@expected["hourly_forecast"][4]["temp"]).to eq(@weather[:hourly][4][:temp])
+    expect(@expected["hourly_forecast"][4]["image"]).to eq(@weather[:hourly][4][:weather][0][:icon])
+    expect(@expected["hourly_forecast"][4]["time"]).to eq(Time.at(@weather[:hourly][4][:dt]).strftime('%l %p'))
 
-    expect(@expected["data"]["attributes"]["weather"]["hourly_forecast"][7]["temp"]).to eq(@weather[:hourly][7][:temp])
-    expect(@expected["data"]["attributes"]["weather"]["hourly_forecast"][7]["image"]).to eq(@weather[:hourly][7][:weather][0][:icon])
-    expect(@expected["data"]["attributes"]["weather"]["hourly_forecast"][7]["time"]).to eq(Time.at(@weather[:hourly][7][:dt]).strftime('%l %p'))
+    expect(@expected["hourly_forecast"][7]["temp"]).to eq(@weather[:hourly][7][:temp])
+    expect(@expected["hourly_forecast"][7]["image"]).to eq(@weather[:hourly][7][:weather][0][:icon])
+    expect(@expected["hourly_forecast"][7]["time"]).to eq(Time.at(@weather[:hourly][7][:dt]).strftime('%l %p'))
   end
 
   it 'only provides 8 hours worth of hourly forecast' do
-    expect(@expected["data"]["attributes"]["weather"]["hourly_forecast"][8]).to eq(nil)
+    expect(@expected["hourly_forecast"][8]).to eq(nil)
   end
 
   it 'provides daily forecast for the next 5 days' do
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][0]["day"]).to eq(Time.at(@weather[:daily][0][:dt]).strftime('%A'))
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][0]["description"]).to eq(@weather[:daily][0][:weather][0][:description])
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][0]["image"]).to eq("http://openweathermap.org/img/wn/#{@weather[:daily][0][:weather][0][:icon]}@2x.png")
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][0]["precipitation"]).to eq(0)
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][0]["high_temp"]).to eq(@weather[:daily][0][:temp][:max])
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][0]["low_temp"]).to eq(@weather[:daily][0][:temp][:min])
+    expect(@expected["daily_forecast"][0]["day"]).to eq(Time.at(@weather[:daily][0][:dt]).strftime('%A'))
+    expect(@expected["daily_forecast"][0]["description"]).to eq(@weather[:daily][0][:weather][0][:description])
+    expect(@expected["daily_forecast"][0]["image"]).to eq("http://openweathermap.org/img/wn/#{@weather[:daily][0][:weather][0][:icon]}@2x.png")
+    expect(@expected["daily_forecast"][0]["precipitation"]).to eq(0)
+    expect(@expected["daily_forecast"][0]["high_temp"]).to eq(@weather[:daily][0][:temp][:max])
+    expect(@expected["daily_forecast"][0]["low_temp"]).to eq(@weather[:daily][0][:temp][:min])
 
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][4]["day"]).to eq(Time.at(@weather[:daily][4][:dt]).strftime('%A'))
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][4]["description"]).to eq(@weather[:daily][4][:weather][0][:description])
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][4]["image"]).to eq("http://openweathermap.org/img/wn/#{@weather[:daily][4][:weather][0][:icon]}@2x.png")
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][4]["precipitation"]).to eq(@weather[:daily][4][:rain])
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][4]["high_temp"]).to eq(@weather[:daily][4][:temp][:max])
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][4]["low_temp"]).to eq(@weather[:daily][4][:temp][:min])
+    expect(@expected["daily_forecast"][4]["day"]).to eq(Time.at(@weather[:daily][4][:dt]).strftime('%A'))
+    expect(@expected["daily_forecast"][4]["description"]).to eq(@weather[:daily][4][:weather][0][:description])
+    expect(@expected["daily_forecast"][4]["image"]).to eq("http://openweathermap.org/img/wn/#{@weather[:daily][4][:weather][0][:icon]}@2x.png")
+    expect(@expected["daily_forecast"][4]["precipitation"]).to eq(@weather[:daily][4][:rain])
+    expect(@expected["daily_forecast"][4]["high_temp"]).to eq(@weather[:daily][4][:temp][:max])
+    expect(@expected["daily_forecast"][4]["low_temp"]).to eq(@weather[:daily][4][:temp][:min])
   end
 
   it 'only provides 5 days worth of daily forecast' do
-    expect(@expected["data"]["attributes"]["weather"]["daily_forecast"][5]).to eq(nil)
+    expect(@expected["daily_forecast"][5]).to eq(nil)
   end
 end
