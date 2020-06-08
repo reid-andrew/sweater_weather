f4d7059ab42fc12d4f79fc80ff4867e187512c88
Andrew Reid <areid.sj@gmail.com>
2020-06-08 09:43:14 -0600

    15 minute commit, partial dev of finding weather by arrival time

--- app/poros/foodie.rb
+++ app/poros/foodie.rb
@@ -3,7 +3,25 @@ class Foodie
     @geocode = GeocodingService.find_geocode(params[:end])
     @end_location = params[:end]
     @travel_time = ''
-    @forecast = ''
+    @forecast = weather(params[:end], date = Time.now)
     @restaurant = ''
   end
+
+  def weather(destination, date)
+    report = WeatherReport.new(destination)
+    get_weather(report, date)
+  end
+
+  def get_weather(report, date)
+    after = report.weather_data[:hourly].select { |hour| hour[:dt] > date.to_i }
+    after.max_by { |x| x[:dt] }
+  end
+
+  # def time_early(date)
+  #   DateTime.new(date.year, date.month, date.day, 0, 0, 0, 0).to_time.to_i
+  # end
+  #
+  # def time_late(date)
+  #   DateTime.new(date.year, date.month, date.day, 24, 0, 0, 0).to_time.to_i
+  # end
 end
