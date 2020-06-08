0a55d3a64249b4308bbf44f242b7cc55be18eaee
Andrew Reid <areid.sj@gmail.com>
2020-06-08 09:50:13 -0600

    Complete forecast methods

--- app/poros/foodie.rb
+++ app/poros/foodie.rb
@@ -1,27 +1,32 @@
 class Foodie
+  attr_reader :id
+
   def initialize(params)
+    @id = "x"
     @geocode = GeocodingService.find_geocode(params[:end])
     @end_location = params[:end]
     @travel_time = ''
-    @forecast = weather(params[:end], date = Time.now)
+    @forecast = forecast(params[:end], date = Time.now)
     @restaurant = ''
+    require "pry"; binding.pry
   end
 
-  def weather(destination, date)
+  private
+
+  def forecast(destination, time)
+    {
+      summary: weather(destination, time)[:weather][0][:description],
+      temperature: weather(destination, time)[:temp]
+    }
+  end
+
+  def weather(destination, time)
     report = WeatherReport.new(destination)
-    get_weather(report, date)
+    get_weather(report, time)
   end
 
-  def get_weather(report, date)
-    after = report.weather_data[:hourly].select { |hour| hour[:dt] > date.to_i }
+  def get_weather(report, time)
+    after = report.weather_data[:hourly].select { |hour| hour[:dt] > time.to_i }
     after.max_by { |x| x[:dt] }
   end
-
-  # def time_early(date)
-  #   DateTime.new(date.year, date.month, date.day, 0, 0, 0, 0).to_time.to_i
-  # end
-  #
-  # def time_late(date)
-  #   DateTime.new(date.year, date.month, date.day, 24, 0, 0, 0).to_time.to_i
-  # end
 end
